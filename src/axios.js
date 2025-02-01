23c21cedcf869bf1e711bb39caa916d75cfe8cc6
Sanket <sanketdisale871@gmail.com>
2025-02-01 16:12:56 +0530

    Added CareerGPT Part

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 const instance = axios.create({
-  //  baseURL: "http://localhost:8000/api/v1",
- baseURL: "https://careerease-website-backend.onrender.com/api/v1",
+  baseURL: "http://localhost:8000/api/v1",
+  //  baseURL: "https://careerease-website-backend.onrender.com/api/v1",
   timeout: 50000,
   headers: {
     "Content-Type": "application/json",
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -101,7 +101,7 @@ const Navbar = () => {
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-dark md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-dark md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700 ">
             {[
               { path: "/dashboard", name: "Dashboard" },
               { path: "/careergpt", name: "CareerGPT" },
